2fee92d48f82191dd3db5499572aefa7192d85ad
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-05 14:27:02 -0700

    dooooooooooooo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 # special imports
 import os
+import time
 # as we make more scenario files add more import's with the file name here!
 import sc1
 
-print("welcome to WAIT!")
-print("choose a number beetween 1-10 and we will navigate you to that scenario")
-num = input("scenario to execute: ")
-f"sc,{num}".scenario()
+print("Welcome to WAIT!")
+print("Choose a number between 1-10 and we will navigate you to that scenario")
+num = input("Scenario to execute: ")
+
+# construct module name
+module_name = f"sc{num}"
+
+# Get the module from globals
+module = globals().get(module_name)
+
+# Check if the module exists
+if module:
+    # Assuming the module has a function named `scenario` that you want to execute
+    module.scenario()
+else:
+    print(f"Scenario {num} not found.")
+    print("please try again!")
+    time.sleep(5)
+    os._exit
