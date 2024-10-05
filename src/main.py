169fc5b1beb2444cfdfcbfeef9d1fc2c18611d5c
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-05 14:00:34 -0700

    add first shiiiiiiiitttttt

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import time
+from datetime import datetime
+
+print("your stuck at a crosswalk and it keeps making you wait when will you be able to go?")
+
+while True:
+    # time
+    current_time = datetime.now()
+    
+    # check time
+    if current_time.hour == 12 and current_time.minute == 0 and current_time.second == 0:
+        print("you waited!")
+        print("if you can cross the whole thing in the next minute then do so! else, come back and WAIT.")
+        time.sleep(10)
+        break
+    
+    # wait another second.
+    time.sleep(1)
