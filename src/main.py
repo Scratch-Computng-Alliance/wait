8e2ab9d5acfcc27488d5b842e731e84a8c3b4fc1
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-05 14:20:09 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# special imports
+import os
+# as we make more scenario files add more import's with the file name here!
+import sc1
+
+print("welcome to WAIT!")
+print("choose a number beetween 1-10 and we will navigate you to that scenario")
+num = input("scenario to execute: ")
+f"sc,{num}".scenario()
