ce017c6afdcc9453d98ebd5ca1b9e6834a5404d5
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-06 08:36:02 -0700

    finish sc2

--- src/sc2.py
+++ src/sc2.py
@@ -1,1 +1,18 @@
-#sc2
+def scenario():
+    print("Your boss said you needed to code something that defines a list and sorts it what do you do?")
+    print("1. make it yourself")
+    print("2. find the answer on stack overflow")
+
+    answer = input("choice:")
+  
+    if answer == "1":
+      print("you take the time to build it and come out with the best possible code:")
+      print("random_numbers = [1, 4, 3, 2]")
+      print("sorted_numbers = sorted(random_numbers)")
+      print("print(sorted_numbers)")
+    else:
+      print("you get it from stack overflow and make this:")
+      print("random_numbers = [1, 4, 3, 2]")
+      print("sorted_numbers = sorted(random_numbers)")
+      print("print(sorted_numbers)")
+      print("but then you get fired from your job. GET OUT!")
