50013f1903e8090c8ab40ba7143c374fe718edf5
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-06 08:38:19 -0700

    add sc2 support here

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ import os
 import time
 # as we make more scenario files add more import's with the file name here!
 import sc1
+import sc2
 
 print("Welcome to WAIT!")
-print("Choose a number between 1-10 and we will navigate you to that scenario")
+print("Choose a number between 1-10 and we will navigate you to that scenario (or type 999 to exit)")
 num = input("Scenario to execute: ")
 
 # construct module name
